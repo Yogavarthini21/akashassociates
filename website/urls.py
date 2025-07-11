9dac2cefab7766df20a86d9ecc22e4d147f44809
Suriya Prakash <sksuriyakannan@gmail.com>
2025-07-11 13:02:47 +0530

    service details page images add

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('contact/',contact,name='contact'),
     path('services/',services,name='services'),
     path('residential/',residential,name='residential'),
+    path('industrial/',industrial,name='industrial'),
+    path('commercial/',commercial,name='commercial'),
+    path('DTCP_approved/',DTCP_approved ,name='DTCP_approved'),
+    path('apartment/',apartment ,name='apartment'),
+    path('renovation/',renovation ,name='renovation'),
 ]
--- website/views.py
+++ website/views.py
@@ -12,3 +12,16 @@ def services(request):
 
 def residential(request):
   return render(request,'residential.html')
+
+def DTCP_approved (request):
+  return render(request,'DTCP_approved .html')
+
+def commercial(request):
+  return render(request,'commercial.html')
+
+def industrial(request):
+  return render(request,'industrial.html')
+def apartment(request):
+  return render(request,'apartment.html')
+def renovation(request):
+  return render(request,'renovation.html')
